8fea52ea78d9a85d7cbc7d1b5b3bb87f8b66f213
kpetrakis <kostispetrak@gmail.com>
2023-08-27 11:34:17 +0300

    day 6 complete

--- 6/sol.py
+++ 6/sol.py
@@ -6,10 +6,14 @@ data = data.strip('\n')
 # print(data)
 
 sop_marker = -1 # start of packet marker position
+som_marker = -1 # start of message marker position
 l = deque() # list holding the 4 most recent letters
+message_l = deque() # list holding the 14 most recent letters
 for i, letter in enumerate(data):
   l.append(letter)
+  message_l.append(letter)
 
+  # find packet marker
   if len(l) == 4:
     s = set(l)
     if len(s) == 4:
@@ -18,4 +22,14 @@ for i, letter in enumerate(data):
     else:
       l.popleft()
 
-print("answer:", sop_marker)
+  # find message marker
+  if len(message_l) == 14:
+    s = set(message_l)
+    if len(s) == 14:
+      # found it
+      som_marker = i + 1 # account for zero indexing
+    else:
+      message_l.popleft()
+
+print("sop marker:", sop_marker)
+print("som marker:", som_marker)
