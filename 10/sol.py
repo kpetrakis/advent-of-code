85f1b65fb8826a45baf0c73dd7da262d9b6bf855
kpetrakis <kostispetrak@gmail.com>
2023-09-12 02:30:05 +0300

    day 10 complete

--- 10/sol.py
+++ 10/sol.py
@@ -2,7 +2,7 @@
 from collections import deque
 from itertools import islice
 
-data = open("ex.txt", 'r').readlines()
+data = open("input.txt", 'r').readlines()
 data = [d.strip('\n') for d in data]
 # print(len(data))
 
@@ -25,3 +25,21 @@ values.appendleft(X) # prosthetw tin arxiki timi tou X - kanei swsti tin arithmi
 answer1 = sum((sum(islice(values, 0, cycle)) * cycle for cycle in cycles_of_interest))
 print("answer part 1:", answer1)
 
+# part2
+print("answer part 2:") # answer = EJCFPGLH
+width, height = 40, 6
+sprite_pos = sum(islice(values, 0, 1)) # initial sprite pos
+for i in range(height):
+  line = []
+  for j in range(width):
+    cycle = i*width + (j+1) #(i + 1) * (j + 1)
+    if sprite_pos - 1 <= j <= sprite_pos + 1:
+      line.append('#');
+    else:
+      line.append('.')
+    sprite_pos = sum(islice(values, 0, cycle + 1))
+    # # print(list(islice(values, 0, cycle)))
+    #print("cycle:", cycle)
+    #print("sprite position:", sprite_pos)
+    #print(''.join(line))
+  print(''.join(line)) # new line
