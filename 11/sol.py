d75b17a18eae1f4009781f88ad3f253bb9993796
kpetrakis <kostispetrak@gmail.com>
2023-09-17 15:46:13 +0300

    day 11 complete

--- 11/sol.py
+++ 11/sol.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import math
 from collections import deque
 from functools import reduce
 
@@ -15,10 +14,10 @@ class Monkey:
       self.items.append(item)
     self.COUNT = 0
   
-  def inspect(self):
+  def inspect(self, mod):
     throwings = list() # list [(worry_level, monkey_idx), ...]
     for item in list(self.items)[:]: # in place copy!
-      worry_level = self.exec_operation(item)
+      worry_level = self.exec_operation(item, mod)
       # print(worry_level)
       self.COUNT += 1
       if worry_level % self.divisible_by == 0:
@@ -31,9 +30,9 @@ class Monkey:
 
     return throwings
 
-  def exec_operation(self, item):
-    old = item
-    return eval(self.operation) // 3
+  def exec_operation(self, item, mod):
+    old = item % mod # before testing worry level
+    return eval(self.operation) #// 3
 
   def receive_item(self, worry_level):
     """
@@ -43,45 +42,42 @@ class Monkey:
 
 
 data = open('input.txt', 'r').read().strip().split('\n')
-# print(data)
 
+mod = 1 # mod all items with that number to keep them small..
 monkeys = list() # all the monkeys 
 for i in range(0, len(data), 7):
   monkey_id = int(data[i].split(' ')[1].strip(':')) 
-  # print(monkey_id)
   items = [int(item.strip(',')) for item in data[i+1].split(' ')[4:]]
-  # print(items)
   operation = ' '.join(data[i+2].strip(' ').split(' ')[3:])
-  # print(operation)
   divisible_by = int(data[i+3].strip(' ').split(' ')[-1]) # test operator
-  # print(divisible_by)
   monkey_if_true = int(data[i+4].strip(' ').split(' ')[-1])
-  # print('monkey true:', monkey_if_true)
   monkey_if_false = int(data[i+5].strip(' ').split(' ')[-1])
-  # print('monkey true:', monkey_if_false)
+
+  mod *= divisible_by # product of all dibisible_by numbers in checks
 
   monkey = Monkey(monkey_id, items, operation, divisible_by, monkey_if_true, monkey_if_false)
   monkeys.append(monkey)
 
 def round():
   for monkey in monkeys:
-    throwings = monkey.inspect() # index of the monkey to throw item
-    # print(throwings)
+    throwings = monkey.inspect(mod) # index of the monkey to throw item
     for worry_level, monkidx in throwings:
       monkeys[monkidx].receive_item(worry_level)
 
-# run 20 rounds
-for r in range(1, 21):
+# run 10000 rounds
+for r in range(1, 10001):
   round()
   # print("round", r)
   # for i, monkey in enumerate(monkeys):
   #   print(i, monkey.items, monkey.COUNT)
   # print("----------------------------")
 
-# after 20 rounds
+# after 10000 rounds
 counts = []
 for i, monkey in enumerate(monkeys):
   counts.append(monkey.COUNT)
-print("answer part 1:", reduce(lambda x, y: x*y, sorted(counts, reverse=True)[:2]))
+
+# print("answer part 1:", reduce(lambda x, y: x*y, sorted(counts, reverse=True)[:2]))
+print("answer part 2:", reduce(lambda x, y: x*y, sorted(counts, reverse=True)[:2]))
 
 
