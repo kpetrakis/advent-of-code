f31bf8fd3d8415a9a1477dacf5923556e866a562
kpetrakis <kostispetrak@gmail.com>
2023-10-02 01:11:08 +0300

    day13 complete

--- 13/sol.py
+++ 13/sol.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from functools import cmp_to_key
 
 def parse(filename):
   with open(filename, 'r') as f:
@@ -44,5 +45,28 @@ def part1():
     index += 1
   return sum_of_indexes
 
+def part2():
+  divider1, divider2 = [[2]], [[6]]
+  packets = []
+  lines = parse('input.txt')
+  for i in range(0, len(lines), 3): # 2
+    packet1 = eval(lines[i])
+    packet2 = eval(lines[i+1])
+
+    packets.append(packet1)
+    packets.append(packet2)
+
+  packets.extend([divider1, divider2])
+  packets.sort(key=cmp_to_key(compare), reverse=True) # check sorting HOWTO
+
+  index1 = packets.index(divider1) + 1
+  index2 = packets.index(divider2) + 1
+  decoder_key = index1 * index2
+  return decoder_key
+    
+
 answer1 = part1()
 print("part1:", answer1)
+
+answer2 = part2()
+print("part2:", answer2)
